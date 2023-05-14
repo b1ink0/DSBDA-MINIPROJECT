fecd35580e70a5766d81c7dbfc8bad483fca4d3a
Aditya Dhade <76063440+b1ink0@users.noreply.github.com>
2023-05-15 03:01:48 +0530

    backend-update

--- Backend/api/app.py
+++ Backend/api/app.py
@@ -8,6 +8,3 @@ app.register_blueprint(models_bp)
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
     return "Hello World!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
